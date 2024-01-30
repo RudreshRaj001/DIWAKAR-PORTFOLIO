833fcc32a6144f473672c4f79cf6cc5502a4d963
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-01-31 00:40:11 +0530

    Fixed Skill Section added more skills

---
+++ SCRIPT/Skill-scroll.js
@@ -0,0 +1,18 @@
+let circularProgress = document.querySelector(".circular-progress"),
+progressValue = document.querySelector(".Circular-bar-progress-value");
+
+let progressStartValue = 0,
+progressEndValue = 70,
+speed = 20;
+
+let progress = setInterval(() => {
+  progressStartValue++;
+  progressValue.textContent = `${progressStartValue}%`;
+
+  circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue*3.6}deg, #ededed 0deg)`;
+
+  if(progressStartValue == progressEndValue){
+    clearInterval(progress);
+  }
+  console.log(progressStartValue);
+}, speed);
